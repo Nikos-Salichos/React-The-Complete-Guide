b559d080a18ad0c70dcb0d116429242f8448af13
Nikos S <niksal1991@gmail.com>
2021-12-20 20:07:41 +0200

    add expense

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,7 @@ function App() {
     { id: 2, title: 'Car insurance 6 Months', amount: 200, date: new Date(2021, 2, 30) },
     { id: 3, title: 'Food Monthly', amount: 200, date: new Date(2021, 2, 25) },
     { id: 4, title: 'Gifts', amount: 40, date: new Date(2021, 2, 22) },
+    { id: 5, title: 'Telephone Bills', amount: 10, date: new Date(2021, 2, 23) },
   ];
 
 
@@ -26,6 +27,7 @@ function App() {
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
       <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+      <ExpenseItem title={expenses[4].title} amount={expenses[4].amount} date={expenses[4].date} />
       <Investment title={investments[0].title} amount={investments[0].amount} date={investments[0].date} />
       <Investment title={investments[1].title} amount={investments[1].amount} date={investments[1].date} />
       <Investment title={investments[2].title} amount={investments[2].amount} date={investments[2].date} />
