f9588b6bd1a24e9ad4aa16537954aa36683b8a17
Nikos S <niksal1991@gmail.com>
2021-12-25 07:45:00 +0200

    Set state

--- my-app/src/components/Expenses/ExpenseItem.js
+++ my-app/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -6,13 +6,23 @@ import Card from '../UI/Card';
 const ExpenseItem = (props) => {
   //props is one parameter that is an object that hold all the attributes as properties
 
+  const [title, setTitle] = useState(props.title); //Hooks must be called directly in component functions
+  //1st name describes the current value
+  //2nd name describes function that updates the value
+
+  const clickHandler = () => {
+    setTitle('Updated');
+    console.log(title) //console.log is not updated the 1st time it changes state, it updates exactly the next
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}€</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
