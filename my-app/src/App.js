aae988c82cadcdae071015c5d335696db11e54a2
Nikos S <niksal1991@gmail.com>
2021-12-26 09:45:44 +0200

    Form Input

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import ExpenseItem from './components/Expenses/ExpenseItem';
-import Investment from './components/Expenses/Investments';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
 
@@ -13,25 +13,15 @@ const App = () => {
   ];
 
 
-  const investments = [
-    { id: 1, title: 'ETFs', amount: 100, date: new Date(2021, 2, 10) },
-    { id: 2, title: 'CryptoCurrencies', amount: 50, date: new Date(2021, 2, 20) },
-    { id: 3, title: 'Stocks', amount: 150, date: new Date(2021, 2, 29) },
-    { id: 4, title: 'Bonds', amount: 30, date: new Date(2021, 2, 30) }
-  ]
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2><NewExpense></NewExpense></h2>
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
       <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
       <ExpenseItem title={expenses[4].title} amount={expenses[4].amount} date={expenses[4].date} />
-      {/* <Investment title={investments[0].title} amount={investments[0].amount} date={investments[0].date} />
-      <Investment title={investments[1].title} amount={investments[1].amount} date={investments[1].date} />
-      <Investment title={investments[2].title} amount={investments[2].amount} date={investments[2].date} />
-      <Investment title={investments[3].title} amount={investments[3].amount} date={investments[3].date} /> */}
     </div>
   );
 }
