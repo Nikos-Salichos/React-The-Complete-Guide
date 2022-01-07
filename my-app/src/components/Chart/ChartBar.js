4c364ca17e34ac7e93355dff4864e3d7d21cdeae
Nikos S <niksal1991@gmail.com>
2022-01-07 18:55:15 +0200

    barFillheight variable definition

--- my-app/src/components/Chart/ChartBar.js
+++ my-app/src/components/Chart/ChartBar.js
@@ -2,6 +2,13 @@ import React from "react";
 import './ChartBar.css'
 
 const ChartBar = props => {
+
+    let barFillheight = '0';
+     
+    if (props.max > 0) {
+        barFillheight = Math.round((props.value / props.maxValue) * 100) + '%';
+    }
+
     return (
         <div className="chart-bar">
             <div className="chart-bar__inner">
